e97972ea390852f0bce615764c7c4ebf5bf09285
Sebastian <sebastian_luis@ciencias.unam.mx>
2021-03-03 17:30:28 -0600

    Hola compañeros

---
+++ Tipos.java
@@ -0,0 +1,33 @@
+public class Tipos{
+
+	public static void main(String[] args){
+
+		double variable;
+
+		int numero;
+
+		int numero1 = 4;
+
+		byte num = -128;
+
+		short n = 32767;
+		
+		long m;
+
+		double d = 3.4;
+
+		float f = 2.3f;
+
+		char c = '9';
+
+		String s = "";
+		
+		int e;
+
+		e=4;
+
+		System.out.println(e);
+
+	}
+
+}
